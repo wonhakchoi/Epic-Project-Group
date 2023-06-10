ff3aa04421cc94b0ac0c354247dcd7c0e8241d3f
Wendy <scw2018@students.cs.ubc.ca>
2023-06-10 09:38:18 -0700

    added add list button to lists page

--- client/src/pages/ResLists.js
+++ client/src/pages/ResLists.js
@@ -1,6 +1,6 @@
 import {useState} from "react";
 import RestaurantList from "../components/RestaurantList";
-import "../components/ResturantList.css"
+import "../components/RestaurantList.css"
 
 // Some sample data, this will need to be stored in database later
 const initialResLists = [{name: "List1"}, {name: "List2"}, {name: "List3"}]
@@ -8,11 +8,17 @@ const initialResLists = [{name: "List1"}, {name: "List2"}, {name: "List3"}]
 // Page for displaying all the user made lists of restaurants
 export default function ResLists() {
     const [resLists, setResLists] = useState(initialResLists);
+    const [listNumber, setListNumber] = useState(initialResLists.length + 1)
 
-    function handleAddResList(newResList) {
+    function handleAddResList() {
+
+        setListNumber(
+            listNumber + 1
+        )
         setResLists([
-            ...resLists, newResList
+            ...resLists, {name: ("List" + listNumber)}
         ])
+
     }
 
     const displayResLists = resLists.map(resList =>
@@ -21,6 +27,9 @@ export default function ResLists() {
     return (
         <div id={"list-container"}>
             {displayResLists}
+            <button id={"add-list-button"} onClick={()=>handleAddResList()}>
+                Add New List
+            </button>
         </div>
     )
 
