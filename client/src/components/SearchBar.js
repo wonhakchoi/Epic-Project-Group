c2057649e69526e998e8a9ba8029522e01593f1b
Tammy <tammykim00723@gmail.com>
2023-06-10 02:13:21 -0700

    delete old code

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,36 +1,3 @@
-// import React, { useState } from 'react';
-// import './SearchBar.css'; // Import the CSS file for styling
-
-// const SearchBar = ({ onSearch }) => {
-//   const [searchTerm, setSearchTerm] = useState('');
-
-//   const handleInputChange = (event) => {
-//     setSearchTerm(event.target.value);
-//   };
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     onSearch(searchTerm);
-//   };
-
-//   return (
-//     <div className="search-bar-container">
-//       <form className="search-bar" onSubmit={handleSubmit}>
-//         <input
-//           type="text"
-//           placeholder="Search..."
-//           value={searchTerm}
-//           onChange={handleInputChange}
-//           className="search-input"
-//         />
-//         <button type="submit" className="search-button">Search</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default SearchBar;
-
 import React, { useState } from 'react';
 import SearchResults from './SearchResults';
 import './SearchBar.css';
