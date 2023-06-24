60be935295a948004d7e02f56268fad82877a3ef
CedricPulmano <pulmano.cedric@gmail.com>
2023-06-23 17:45:01 -0700

    updated imports to users folder

--- client/src/pages/Friends.js
+++ client/src/pages/Friends.js
@@ -1,7 +1,7 @@
 import React from "react";
-import FriendsList from "../components/friends/FriendsList";
-import FriendSearch from "../components/friends/FriendSearch";
-import FriendRequests from "../components/friends/FriendRequests";
+import FriendsList from "../components/users/FriendsList";
+import FriendSearch from "../components/users/FriendSearch";
+import FriendRequests from "../components/users/FriendRequests";
 
 const Friends = () => {
     return (
