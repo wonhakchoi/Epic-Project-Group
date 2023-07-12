9e3ffddcf4fe5ea8536c97a77ae2bcf47ce5d91d
CedricPulmano <pulmano.cedric@gmail.com>
2023-07-11 18:47:24 -0700

    Explicitly declared which collection

--- server/models/restaurantModel.js
+++ server/models/restaurantModel.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose");
 
-const restaurantSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+const restaurantSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        openingHours: {
+            type: String,
+            required: true,
+        },
     },
-    description: {
-        type: String,
-        required: true,
-    },
-    location: {
-        type: String,
-        required: true,
-    },
-    openingHours: {
-        type: String,
-        required: true,
-    },
-});
+    { collection: "restaurants" }
+);
 
 const Restaurant = mongoose.model("Restaurant", restaurantSchema);
 
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,30 +1,33 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        biography: {
+            type: String,
+        },
+        ratedRestaurants: {
+            type: Map,
+            of: Number,
+        },
+        friends: {
+            type: [String],
+            default: [],
+        },
+        outgoingRequests: {
+            type: [String],
+            default: [],
+        },
+        incomingRequests: {
+            type: [String],
+            default: [],
+        },
     },
-    biography: {
-        type: String,
-    },
-    ratedRestaurants: {
-        type: Map,
-        of: Number,
-    },
-    friends: {
-        type: [String],
-        default: [],
-    },
-    outgoingRequests: {
-        type: [String],
-        default: [],
-    },
-    incomingRequests: {
-        type: [String],
-        default: [],
-    },
-});
+    { collection: "users" }
+);
 
 const User = mongoose.model("User", userSchema);
 
