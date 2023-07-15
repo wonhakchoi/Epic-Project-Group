d0ac74dff72fb351efcf1623cd7778607a3feda6
wonhakchoi <wonhakchoi@gmail.com>
2023-07-15 10:48:44 -0400

    set up auth, fixed bug

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -1,17 +1,67 @@
 // actions/authActions.js
-export const login = (email, password) => {
-    // Perform login logic here (API request)
-    return {
-        type: 'LOGIN',
-        payload: { email, password },
+// export const login = (email, password) => {
+//     // Perform login logic here (API request)
+//     return {
+//         type: 'LOGIN',
+//         payload: { email, password },
+//     };
+// };
+
+// // actions/signupActions.js
+// export const signup = (email, password) => {
+//     // Perform signup logic here (API request)
+//     return {
+//         type: 'SIGNUP',
+//         payload: { email, password },
+//     };
+// };
+
+import axios from 'axios';
+
+export const signup = (email, password) => {
+    return dispatch => {
+        axios
+            .post('http://localhost:3001/auth/signup', { email, password })
+            .then(response => {
+                if (response == "exist") {
+                    console.log("user exists");
+                    throw new Error("User already exists!");
+                }
+                dispatch({ type: 'SIGNUP_SUCCESS', payload: response.data });
+            }
+            )
+            .catch(error => {
+                console.log('failed sign up');
+                console.log(error);
+                // dispatch({ type: 'SIGNUP_FAILURE', payload: error.response.data.error });
+                dispatch({ type: 'SET_MESSAGE', payload: error.message });
+                // dispatch({ type: 'SIGNUP_FAILURE', payload: error.message });
+            });
     };
 };
 
-// actions/signupActions.js
-export const signup = (email, password) => {
-    // Perform signup logic here (API request)
-    return {
-        type: 'SIGNUP',
-        payload: { email, password },
+export const login = (email, password) => {
+    return dispatch => {
+        axios
+            .post('http://localhost:3001/auth/login', { email, password })
+            .then(response => {
+                dispatch({ type: 'LOGIN_SUCCESS', payload: response.data });
+            })
+            .catch(error => {
+                dispatch({ type: 'LOGIN_FAILURE', payload: error.response.data.error });
+            });
     };
 };
+
+export const logout = () => {
+    return { type: 'LOGOUT' };
+};
+
+export const setMessage = (message) => ({
+    type: 'SET_MESSAGE',
+    payload: message,
+});
+
+export const clearMessage = () => ({
+    type: 'CLEAR_MESSAGE',
+});
--- client/src/redux/reducers/authentication.js
+++ client/src/redux/reducers/authentication.js
@@ -1,42 +1,101 @@
+// const initialState = {
+//     loggedIn: true,
+//     user: "64a20849b5b47429af1b7900",
+//     error: null,
+// };
+
+// const authentication = (state = initialState, action) => {
+//     switch (action.type) {
+//         case "LOGIN":
+//             const { email: loginEmail, password: loginPassword } = action.payload;
+//             // Perform login logic here (verify credentials, make API requests)
+//             // Update the state based on the login result
+
+//             // const loginSuccessful = verifyCredentials(loginEmail, loginPassword);
+//             const loginSuccessful = true;
+
+//             if (loginSuccessful) {
+//                 return {
+//                     loggedIn: true,
+//                     user: loginEmail,
+//                 };
+//             } else {
+//                 return state; // Handle login failure
+//             }
+//         case "SIGNUP":
+//             const { email: signupEmail, password: signupPassword } = action.payload;
+//             // Perform signup logic here (make API requests)
+//             // Update the state based on the signup result
+
+//             // const signupSuccessful = registerUser(signupEmail, signupPassword);
+//             const signupSuccessful = true;
+
+//             if (signupSuccessful) {
+//                 return {
+//                     loggedIn: true,
+//                     user: signupEmail,
+//                 };
+//             } else {
+//                 return state; // Handle signup failure
+//             }
+//         default:
+//             return state;
+//     }
+// };
+
+// export default authentication;
+
+// !! =================== 
 const initialState = {
     loggedIn: true,
     user: "64a20849b5b47429af1b7900",
+    error: null,
+    // isAuthenticated: false,
 };
 
 const authentication = (state = initialState, action) => {
     switch (action.type) {
-        case "LOGIN":
-            const { email: loginEmail, password: loginPassword } = action.payload;
-            // Perform login logic here (verify credentials, make API requests)
-            // Update the state based on the login result
-
-            // const loginSuccessful = verifyCredentials(loginEmail, loginPassword);
-            const loginSuccessful = true;
-
-            if (loginSuccessful) {
-                return {
-                    loggedIn: true,
-                    user: loginEmail,
-                };
-            } else {
-                return state; // Handle login failure
-            }
-        case "SIGNUP":
-            const { email: signupEmail, password: signupPassword } = action.payload;
-            // Perform signup logic here (make API requests)
-            // Update the state based on the signup result
-
-            // const signupSuccessful = registerUser(signupEmail, signupPassword);
-            const signupSuccessful = true;
-
-            if (signupSuccessful) {
-                return {
-                    loggedIn: true,
-                    user: signupEmail,
-                };
-            } else {
-                return state; // Handle signup failure
-            }
+        case 'SIGNUP_SUCCESS':
+            return {
+                ...state,
+                user: action.payload,
+                error: null,
+                loggedIn: true,
+            };
+        case 'SIGNUP_FAILURE':
+            return {
+                ...state,
+                error: action.payload,
+            };
+        case 'LOGIN_SUCCESS':
+            return {
+                ...state,
+                user: action.payload,
+                error: null,
+                loggedIn: true,
+            };
+        case 'LOGIN_FAILURE':
+            return {
+                ...state,
+                error: action.payload,
+            };
+        case 'LOGOUT':
+            return {
+                ...state,
+                user: null,
+                error: null,
+                loggedIn: false,
+            };
+        case 'SET_MESSAGE':
+            return {
+                ...state,
+                error: action.payload,
+            };
+        case 'CLEAR_MESSAGE':
+            return { 
+                ...state,
+                error: null ,
+            };
         default:
             return state;
     }
