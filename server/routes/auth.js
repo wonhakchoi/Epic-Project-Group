eb279c61516376b8392262708725686fd4e19839
Wendy <scw2018@students.cs.ubc.ca>
2023-07-29 06:45:30 -0700

    change to deployed frontend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,7 +9,7 @@ router.options('*', cors())
 router.use(function (req, res, next) {
     // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
     // res.header("Access-Control-Allow-Origin", /\.easy-eats-frontend.onrender\.com$/); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://easy-eats-frontend.onrender.com"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
