a993b0061364dab6670f27726f9d186622b8822f
Malcolm Zhao <96398778+ZhaoHanrui@users.noreply.github.com>
2023-07-29 08:14:03 -0700

    Maps: Styling, error handling, photos API, (#54)

--- server/routes/maps_api.js
+++ server/routes/maps_api.js
@@ -7,13 +7,18 @@ require("dotenv").config();
 router.get("/:searchTerm", async (req, res, next) => {
     let searchTerm = req.params.searchTerm;
     searchTerm = searchTerm.replace(/ /g, '+');
-    try {
-        console.log(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=vancouver+${searchTerm}&type=food&key=${process.env.GOOGLE_PLACES_API_KEY}`);
-        const {data} = await axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=vancouver+${searchTerm}&type=food&key=${process.env.GOOGLE_PLACES_API_KEY}`);
-        res.json(data);
-    } catch (err) {
-        next(err);
+    const {data} = await axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=vancouver+${searchTerm}&type=food&key=${process.env.GOOGLE_PLACES_API_KEY}`);
+    for (let i = 0; i < data.results.length; i++) {
+        if ("photos" in data.results[i]) {
+            let imageURL = await axios.get(
+                `https://maps.googleapis.com/maps/api/place/photo?photoreference=${data.results[i].photos[0].photo_reference}&maxheight=500&maxwidth=500&key=${process.env.GOOGLE_PLACES_API_KEY}` 
+            );
+            data.results[i]["picture_icon"] = imageURL.config.url;
+        } else {
+            data.results.pop(i);
+        }
     }
+    res.json(data);
 });
 
 module.exports = router;
