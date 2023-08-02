42349130b755cbfe20c5f6878752e233734c86cd
Wendy <scw2018@students.cs.ubc.ca>
2023-08-02 15:46:04 -0700

    create env files for url

--- client/src/redux/services/ratingsService.js
+++ client/src/redux/services/ratingsService.js
@@ -1,18 +1,14 @@
 import axios from "axios";
 
-const baseRatingUrl = "http://localhost:3001/ratings";
-// const baseRatingUrl = "https://easy-eats-backend-9u5y.onrender.com/ratings";
-// const baseRatingUrl = "https://easy-eats-backend-local.onrender.com/ratings";
-
 // gets the next subset of ratings
 const getRatings = async (skipAmount, resultsToGet) => {
-    const response = await axios.get(`${baseRatingUrl}/allRatings/${skipAmount}/${resultsToGet}`);
+    const response = await axios.get(`${process.env.REACT_APP_BACKEND}/ratings/allRatings/${skipAmount}/${resultsToGet}`);
     return response;
 };
 
 // gets the next subset of friend ratings
 const getFriendRatings = async (skipAmount, resultsToGet, friendIDs) => {
-    const response = await axios.get(`${baseRatingUrl}/friendRatings/${skipAmount}/${resultsToGet}`, {
+    const response = await axios.get(`$${process.env.REACT_APP_BACKEND}/ratings/friendRatings/${skipAmount}/${resultsToGet}`, {
         params: { friendIDs: friendIDs },
     });
     console.log(friendIDs);
@@ -21,31 +17,31 @@ const getFriendRatings = async (skipAmount, resultsToGet, friendIDs) => {
 
 // gets the ratings of a specific user
 const getUserRatings = async (userID) => {
-    const userRatings = await axios.get(`${baseRatingUrl}/userRatings/${userID}`);
+    const userRatings = await axios.get(`${process.env.REACT_APP_BACKEND}/ratings/userRatings/${userID}`);
     return userRatings;
 };
 
 // gets the ratings of a specific restaurant
 const getRestaurantRatings = async (restaurantID) => {
-    const restaurantRatings = await axios.get(`${baseRatingUrl}/restaurantRatings/${restaurantID}`);
+    const restaurantRatings = await axios.get(`${process.env.REACT_APP_BACKEND}/ratings/restaurantRatings/${restaurantID}`);
     return restaurantRatings;
 };
 
 // creates a new user rating on a specific restaurant
 const postUserRatings = async (userID, restaurantID, body) => {
-    const addedUserRating = await axios.post(`${baseRatingUrl}/${userID}/${restaurantID}`, body);
+    const addedUserRating = await axios.post(`${process.env.REACT_APP_BACKEND}/ratings/${userID}/${restaurantID}`, body);
     return addedUserRating;
 };
 
 // updates an existing rating
 const updateRatings = async (ratingID, body) => {
-    const updatedRating = await axios.put(`${baseRatingUrl}/${ratingID}`, body);
+    const updatedRating = await axios.put(`${process.env.REACT_APP_BACKEND}/ratings/${ratingID}`, body);
     return updatedRating;
 };
 
 // deletes an existing rating
 const deleteRatings = async (ratingID) => {
-    const deletedRating = await axios.delete(`${baseRatingUrl}/${ratingID}`);
+    const deletedRating = await axios.delete(`${process.env.REACT_APP_BACKEND}/ratings/${ratingID}`);
     return deletedRating;
 };
 
