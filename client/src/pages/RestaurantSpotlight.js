1f0d9805a6aa30a8e626b34e1d0f2535ff52b9ec
CedricPulmano <pulmano.cedric@gmail.com>
2023-08-02 15:53:02 -0700

    Ability to see ratings for specific restaurant

--- client/src/pages/RestaurantSpotlight.js
+++ client/src/pages/RestaurantSpotlight.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import RatingCard from "../components/ratings/RatingCard";
 import { getRestaurantByPlaceID } from "../redux/services/mapService";
+import RatingService from "../redux/services/ratingsService";
 
 const RestaurantSpotlight = () => {
     const { placeID } = useParams();
     const [restaurant, setRestaurant] = useState(null);
+    const [ratings, setRatings] = useState(null);
+    const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
         const fetchRestaurantData = async () => {
             try {
-                const restaurantData = await getRestaurantByPlaceID(placeID);
-                setRestaurant(restaurantData.data);
-                console.log(restaurant);
+                const [restaurantData, ratingData] = await Promise.all([
+                    getRestaurantByPlaceID(placeID),
+                    RatingService.getRestaurantRatings(placeID),
+                ]);
+                setRestaurant(restaurantData.data.result);
+                setRatings(ratingData.data);
+                setLoaded(true);
             } catch (error) {
                 console.error(error);
             }
@@ -20,9 +28,34 @@ const RestaurantSpotlight = () => {
         fetchRestaurantData();
     }, [placeID]);
 
+    useEffect(() => {
+        console.log("RESTAURANT");
+        console.log(restaurant);
+    }, [restaurant]);
+
+    useEffect(() => {
+        console.log("RATINGS");
+        console.log(ratings);
+    }, [ratings]);
+
     return (
         <div>
-            <h1>RESTAURANT SPOTLIGHT {placeID}</h1>
+            {loaded && (
+                <section>
+                    <h1>{restaurant.name}</h1>
+                    {ratings.map((rating) => (
+                        <RatingCard
+                            key={rating._id}
+                            id={rating._id}
+                            name={rating.userID}
+                            restaurant={rating.restaurantID}
+                            score={rating.score}
+                            comment={rating.comments ? rating.comments : ""}
+                            date={rating.updatedAt}
+                        />
+                    ))}
+                </section>
+            )}
         </div>
     );
 };
