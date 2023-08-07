3b9aae93216941cc9bb3ad1c23bdef4c2eeb5538
wonhakchoi <wonhakchoi@gmail.com>
2023-08-07 11:16:02 -0400

    cleaned up code

--- client/src/components/ratings/RatingCard.js
+++ client/src/components/ratings/RatingCard.js
@@ -1,38 +1,20 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { Rating, Typography, Grid, Container } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { Rating, Typography, Grid, Container, CardMedia } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+
 
 // import img from "../../../public/images/home-icons/pasta";
 
 const RatingCard = ({ id, name, restaurant, score, comment, date }) => {
-    // const icons = useSelector((state) => state.users.iconLocations);
-    // const restaurantsSlice = useSelector((state) => state.restaurants.allRestaurants);
-    // const authenticationSlice = useSelector((state) => state.authentication.authentication);
     const dispatch = useDispatch();
     const icons = useSelector((state) => state.users.iconLocations);
 
     // https://mui.com/material-ui/react-card/
     return (
-        // <div>
-        //     <section>
-        //         <Typography component="legend">Rating</Typography>
-        //         <Rating name="read-only" value={score} readOnly precision={0.5}/>
-        //         <h3>{name}</h3>
-        //     </section>
-        // </div>
         <Grid
             container
             direction="column"
@@ -45,6 +27,7 @@ const RatingCard = ({ id, name, restaurant, score, comment, date }) => {
                     <CardHeader
                         avatar={
                             <img className="user-icon" src={icons[Math.floor(Math.random() * icons.length)]} alt={name} />
+                            // <img className="user-icon" src={icons[Math.floor(Math.random() * icons.length)]} alt={name} />
                         }
                         title={
                             <Typography mr={6} gutterBottom variant="h6" component="h2">
