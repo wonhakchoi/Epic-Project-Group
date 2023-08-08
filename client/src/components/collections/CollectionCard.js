ebe9ec54be36d3563e6994de162b969544975cd2
Tammy <tammykim00723@gmail.com>
2023-08-08 14:37:05 -0700

    Styled profile, search, collection pages

--- client/src/components/collections/CollectionCard.js
+++ client/src/components/collections/CollectionCard.js
@@ -1,99 +1,108 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
-    Card,
-    CardContent,
-    CardMedia,
-    Typography,
-    IconButton,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  IconButton,
 } from "@mui/material";
-import PushPinIcon from '@mui/icons-material/PushPin';
-import RemoveIcon from '@mui/icons-material/Remove';
-import {useDispatch, useSelector} from "react-redux";
-import {getCollectionsAsync, patchCollectionPinAsync} from "../../redux/thunks/collectionsThunks";
+import PushPinIcon from "@mui/icons-material/PushPin";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getCollectionsAsync,
+  patchCollectionPinAsync,
+} from "../../redux/thunks/collectionsThunks";
 import CardActions from "@mui/material/CardActions";
 
 // Component for each Collection Card
-export default function CollectionCard({collection}) {
-    const dispatch = useDispatch();
-    const currUser = useSelector((state) => state.sauth.currUser);
+export default function CollectionCard({ collection }) {
+  const dispatch = useDispatch();
+  const currUser = useSelector((state) => state.sauth.currUser);
 
-    const handlePin = () => {
-        dispatch(patchCollectionPinAsync({collectionId: collection._id, isPinned: true}))
-            .then(() => {
-                dispatch(getCollectionsAsync(currUser));
-            });
-    }
+  const handlePin = () => {
+    dispatch(
+      patchCollectionPinAsync({ collectionId: collection._id, isPinned: true })
+    ).then(() => {
+      dispatch(getCollectionsAsync(currUser));
+    });
+  };
 
-    const handleUnpin = () => {
-        dispatch(patchCollectionPinAsync({collectionId: collection._id, isPinned: false}))
-            .then(() => {
-                dispatch(getCollectionsAsync(currUser));
-            });
-    }
+  const handleUnpin = () => {
+    dispatch(
+      patchCollectionPinAsync({ collectionId: collection._id, isPinned: false })
+    ).then(() => {
+      dispatch(getCollectionsAsync(currUser));
+    });
+  };
 
-    return (
-        <div style={{position: "relative"}}>
-            <Card
+  return (
+    <div style={{ position: "relative" }}>
+      <Card
+        sx={{
+          maxWidth: "100vh",
+          margin: "4vh auto 4vh auto",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          "&:hover": {
+            backgroundColor: "#e1e1e1",
+          },
+        }}
+      >
+        <CardActions>
+          {!collection.pinned && (
+            <IconButton color="default" onClick={handlePin} >
+              <PushPinIcon
                 sx={{
-                    maxWidth: 300,
-                    margin: "50px auto 30px auto",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    "&:hover": {
-                        backgroundColor: "#e1e1e1",
-                    },
+                  fontSize: "3vh",
                 }}
+              />
+            </IconButton>
+          )}
+          {collection.pinned && (
+            <IconButton
+              color="default"
+              onClick={handleUnpin}
             >
-                <CardActions>
-                    {!collection.pinned && (
-                        <IconButton
-                            color="default"
-                            onClick={handlePin}
-                            // sx={{position: "relative", top: "5px", right: "5px"}}
-                        >
-                            <PushPinIcon/>
-                            {/* Commenting this out because the icon looks awkward, will fix this later */}
-                        </IconButton>
-                    )}
-                    {collection.pinned && (
-                        <IconButton
-                            color="default"
-                            onClick={handleUnpin}
-                            // sx={{position: "relative", top: "5px", right: "5px"}}
-                        >
-                            <RemoveIcon/>
-                            {/* Commenting this out because the icon looks awkward, will fix this later */}
-                        </IconButton>
-                    )}
-                </CardActions>
+              <RemoveIcon  sx={{
+                fontSize: "3vh",
+              }} />
+            </IconButton>
+          )}
+        </CardActions>
 
-                <Link to={"/collections/" + collection._id} style={{textDecoration: "none"}}>
-                    <CardMedia
-                        component="img"
-                        alt="Collection Image"
-                        height="200"
-                        image={collection.img}
-                        sx={{objectFit: "cover"}}
-                    />
-                    <CardContent>
-                        <Link to={"/collections/" + collection._id} style={{textDecoration: "none"}}>
-                            <Typography variant="h6" component="div" sx={{textDecoration: "none"}}>
-                                {collection.name}
-                            </Typography>
-                        </Link>
-                    </CardContent>
-                </Link>
-                {/* <IconButton
-          color={isPinned ? "primary" : "default"}
-          onClick={handlePinClick}
-          sx={{ position: "absolute", top: "5px", right: "5px" }}
+        <Link
+          to={"/collections/" + collection._id}
+          style={{ textDecoration: "none" }}
         >
-          <PushPinIcon />
-        </IconButton> */}
-
-            </Card>
-        </div>
-    );
+          <CardMedia
+            component="img"
+            alt="Collection Image"
+            height="200vh"
+            image={collection.img}
+            sx={{ objectFit: "cover" }}
+          />
+          <CardContent>
+            <Link
+              to={"/collections/" + collection._id}
+              style={{ textDecoration: "none" }}
+            >
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{
+                  textDecoration: "none",
+                  fontSize: "2vh",
+                }}
+              >
+                {collection.name}
+              </Typography>
+            </Link>
+          </CardContent>
+        </Link>
+      </Card>
+    </div>
+  );
 }
--- client/src/components/restaurants/Restaurant.js
+++ client/src/components/restaurants/Restaurant.js
@@ -1,108 +1,170 @@
 import React, { useState } from "react";
 import "./Restaurant.css";
 import { useDispatch } from "react-redux";
-import { displayAddToCollection, setRestaurant } from "../../redux/reducers/collectionPopupSlice";
+import {
+  displayAddToCollection,
+  setRestaurant,
+} from "../../redux/reducers/collectionPopupSlice";
 import { LeaveReviewModal } from "../ratings/LeaveReviewModal";
 import { Typography, Box, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import { clearError } from "../../redux/reducers/postRatingSlice";
 
 const Restaurant = ({ restaurant }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const [showReviewModal, setShowReviewModal] = useState(false);
+  const [showReviewModal, setShowReviewModal] = useState(false);
 
-    const openModal = () => {
-        setShowReviewModal((showReviewModal) => !showReviewModal);
-        dispatch(clearError());
-    };
+  const openModal = () => {
+    setShowReviewModal((showReviewModal) => !showReviewModal);
+    dispatch(clearError());
+  };
 
-    const handleAddToCollection = () => {
-        dispatch(displayAddToCollection());
-        dispatch(setRestaurant(restaurant));
-    };
+  const handleAddToCollection = () => {
+    dispatch(displayAddToCollection());
+    dispatch(setRestaurant(restaurant));
+  };
 
-    const { name, formatted_address, opening_hours, rating, user_ratings_total, picture_icon, types } = restaurant;
-    if (name && formatted_address && opening_hours && rating && user_ratings_total && picture_icon) {
-        if (!types.includes("food") && !types.includes("restaurant")) {
-            return <></>;
-        }
-        let YesOrNo = <span className="midString">Undisclosed</span>;
-        let ratingWithColour;
-        if ("open_now" in opening_hours) {
-            opening_hours["open_now"]
-                ? (YesOrNo = <span className="yesString">Yes</span>)
-                : (YesOrNo = <span className="noString">No</span>);
-        } else {
-            console.log(opening_hours);
-        }
-        rating < 2
-            ? (ratingWithColour = <span className="noString">{rating}</span>)
-            : rating < 4
-            ? (ratingWithColour = <span className="midString">{rating}</span>)
-            : (ratingWithColour = <span className="yesString">{rating}</span>);
-        return (
-            <Box sx={{ marginLeft: "80px", marginRight: "80px" }}>
-                <div className={"restaurant-card"}>
-                    <img className="restaurant-img" src={picture_icon} alt="URL not found"></img>
-                    <div className="descriptor">
-                        <Typography variant="h5" component="h3" sx={{ fontFamily: "Arial, sans-serif" }}>
-                            {name}
-                        </Typography>
-                        <Typography variant="body1" className="info">
-                            <span className="formatted_address">{formatted_address}</span>
-                        </Typography>
-                        <Typography variant="body1" className="info">
-                            <span className="opening-hours">Open Now? {YesOrNo}</span>
-                        </Typography>
-                        <Typography variant="body1" className="info">
-                            Rating: {ratingWithColour} by <span className="rating">{user_ratings_total}</span> users
-                        </Typography>
-                        <Box sx={{ display: "flex", gap: 2 }}>
-                            <Button
-                                sx={{
-                                    backgroundColor: "#FFF4BB",
-                                    color: "#000000",
-                                    fontFamily: "Arial, sans-serif",
-                                    marginTop: "30px",
-                                }}
-                                variant="contained"
-                                onClick={handleAddToCollection}
-                            >
-                                Add to Collection
-                            </Button>
-                            <Button
-                                sx={{
-                                    backgroundColor: "#000000",
-                                    fontFamily: "Arial, sans-serif",
-                                    marginTop: "30px",
-                                }}
-                                variant="contained"
-                                onClick={openModal}
-                            >
-                                Leave Review
-                            </Button>
-                            <Link to={`/restaurants/${restaurant.place_id}`}>
-                                <Button
-                                    sx={{
-                                        backgroundColor: "#0047AB",
-                                        fontFamily: "Arial, sans-serif",
-                                        marginTop: "30px",
-                                    }}
-                                    variant="contained"
-                                >
-                                    View Restaurant
-                                </Button>
-                            </Link>
-                        </Box>
-                        <LeaveReviewModal showReviewModal={showReviewModal} setShowReviewModal={setShowReviewModal} placeID={restaurant.place_id} restaurantName={name} />
-                    </div>
-                </div>
-            </Box>
-        );
-    } else {
-        return <></>;
+  const {
+    name,
+    formatted_address,
+    opening_hours,
+    rating,
+    user_ratings_total,
+    picture_icon,
+    types,
+  } = restaurant;
+  if (
+    name &&
+    formatted_address &&
+    opening_hours &&
+    rating &&
+    user_ratings_total &&
+    picture_icon
+  ) {
+    if (!types.includes("food") && !types.includes("restaurant")) {
+      return <></>;
     }
+    let YesOrNo = <span className="midString">Undisclosed</span>;
+    let ratingWithColour;
+    if ("open_now" in opening_hours) {
+      opening_hours["open_now"]
+        ? (YesOrNo = <span className="yesString">Yes</span>)
+        : (YesOrNo = <span className="noString">No</span>);
+    } else {
+      console.log(opening_hours);
+    }
+    rating < 2
+      ? (ratingWithColour = <span className="noString">{rating}</span>)
+      : rating < 4
+      ? (ratingWithColour = <span className="midString">{rating}</span>)
+      : (ratingWithColour = <span className="yesString">{rating}</span>);
+    return (
+      <Box
+        sx={{
+          marginLeft: "3vh",
+          marginRight: "3vh",
+          boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.16)",
+        }}
+      >
+        <div className={"restaurant-card"}>
+          <img
+            className="restaurant-img"
+            src={picture_icon}
+            alt="URL not found"
+            style={{ width: "30vh", height: "30vh" }}
+          ></img>
+          <div className="descriptor">
+            <Typography
+              variant="h5"
+              component="h3"
+              sx={{ fontSize: "3vh", fontFamily: "Arial, sans-serif" }}
+            >
+              {name}
+            </Typography>
+            <Typography
+              variant="body1"
+              className="info"
+              sx={{ fontSize: "2.2vh" }}
+            >
+              <span className="formatted_address">{formatted_address}</span>
+            </Typography>
+            <Typography
+              variant="body1"
+              className="info"
+              sx={{ fontSize: "2.2vh" }}
+            >
+              <span className="opening-hours">Open Now? {YesOrNo}</span>
+            </Typography>
+            <Typography
+              variant="body1"
+              className="info"
+              sx={{ fontSize: "2.2vh" }}
+            >
+              Rating: {ratingWithColour} by{" "}
+              <span className="rating">{user_ratings_total}</span> users
+            </Typography>
+            <Box sx={{ display: "flex", gap: 2 }}>
+              <Button
+                sx={{
+                  fontSize: "1.6vh",
+                  backgroundColor: "#9665D1",
+                  fontFamily: "Arial, sans-serif",
+                  marginTop: "3vh",
+                  "&:hover": {
+                    backgroundColor: "#432C8F",
+                  },
+                }}
+                variant="contained"
+                onClick={handleAddToCollection}
+              >
+                Add to Collection
+              </Button>
+              <Button
+                sx={{
+                  fontSize: "1.6vh",
+                  backgroundColor: "#9665D1",
+                  fontFamily: "Arial, sans-serif",
+                  marginTop: "3vh",
+                  "&:hover": {
+                    backgroundColor: "#432C8F",
+                  },
+                }}
+                variant="contained"
+                onClick={openModal}
+              >
+                Leave Review
+              </Button>
+              <Link to={`/restaurants/${restaurant.place_id}`}>
+                <Button
+                  sx={{
+                    fontSize: "1.6vh",
+                    backgroundColor: "#9665D1",
+                    fontFamily: "Arial, sans-serif",
+                    marginTop: "3vh",
+                    "&:hover": {
+                      backgroundColor: "#432C8F",
+                    },
+                  }}
+                  variant="contained"
+                >
+                  View Restaurant
+                </Button>
+              </Link>
+            </Box>
+            <LeaveReviewModal
+              showReviewModal={showReviewModal}
+              setShowReviewModal={setShowReviewModal}
+              placeID={restaurant.place_id}
+              restaurantName={name}
+            />
+          </div>
+        </div>
+      </Box>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default Restaurant;
