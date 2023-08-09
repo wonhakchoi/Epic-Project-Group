7b453d6724c956c8afed22382965d2ca5d94493d
Wendy <scw2018@students.cs.ubc.ca>
2023-08-09 07:21:24 -0700

    fix being able to edit other pages from friends

--- client/src/components/restaurants/RestaurantCard.js
+++ client/src/components/restaurants/RestaurantCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteRestaurantCollectionAsync } from "../../redux/thunks/collectionsThunks";
 import { Box, Typography, Button } from "@mui/material";
@@ -7,6 +7,8 @@ export default function RestaurantCard({ restaurant }) {
   const collectionDetails = useSelector(
     (state) => state.collections.currCollectionDetails
   );
+  const [isCurrUser, setIsCurrUser] = useState(false);
+  const currUser = useSelector((state) => state.sauth.currUser);
   const { place_id, name, formatted_address, opening_hours, rating, user_ratings_total } = restaurant;
   let YesOrNo;
   let ratingWithColour;
@@ -18,10 +20,47 @@ export default function RestaurantCard({ restaurant }) {
     : (ratingWithColour = <span className="yesString">{rating}</span>);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+      if (currUser === collectionDetails.userId) {
+          setIsCurrUser(true);
+      } else {
+          setIsCurrUser(false);
+      }
+  }, [currUser, collectionDetails])
+
   function handleRemove() {
     let cId = collectionDetails._id;
     dispatch(deleteRestaurantCollectionAsync({ collectionId: cId, restaurantId: place_id }));
     window.location.reload();
+  }
+
+  if (!isCurrUser) {
+      return (
+          <Box sx={{
+              backgroundColor: "#f5f5f5",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+              padding: "1rem",
+              marginBottom: "1rem",
+              borderColor: "#ddd",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+          }}>
+              <Typography variant="h5" component="h3" sx={{ marginBottom: "0.5rem" }}>
+                  {name}
+              </Typography>
+              <Typography variant="body1" className="info" sx={{ marginBottom: "0.5rem" }}>
+                  <span className="formatted_address">{formatted_address}</span>
+              </Typography>
+              <Typography variant="body1" className="info" sx={{ marginBottom: "0.5rem" }}>
+                  Open Now? {YesOrNo}
+              </Typography>
+              <Typography variant="body1" className="info" sx={{ marginBottom: "0.5rem" }}>
+                  Rating: {ratingWithColour} by <span className="rating">{user_ratings_total}</span> users
+              </Typography>
+          </Box>
+      )
   }
 
   return (
